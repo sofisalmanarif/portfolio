63d02c8b19233cf877b1984a6a7f30f9eead444d
sofisalmanarif <sofisalman9906@gmail.com>
2024-10-26 15:42:53 +0530

    hero section

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,27 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <div>
-    hay
-  </div>
+  <main className="container text-balance w-full flex-wrap flex mx-auto max-w-[800px]">
+    <section className="flex bg-red-500 pb-10 items-center w-full justify-between  ">
+      <div className="mt-10">
+      <h1 className="title text-5xl font-[heading] font-bold">hi Salman here 👋</h1>
+      <p className="font-light mt-4">20-year-old  software developer from India 🇮🇳</p>
+      <p className="font-light mt-4 ">I like to develop full-stack, drink instant coffee and get coding<br/> advice from my cat, Luffy.</p>
+      <section className="mt-8 border-[1px] w-fit px-4 py-[4px] hover:bg-zinc-800 rounded-md border-zinc-700">
+
+        <button>
+          Resume
+        </button>
+        <div>
+          
+        </div>
+      </section>
+      </div>
+      <div className="px-2">
+        <Image width={250} height={200} className="rounded-xl min-h-40 " src={"/profile.jpeg"}/>
+      </div>
+    </section>
+
+  </main>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <header className='sticky top-0 py-6 px-2 backdrop-blur-lg'>
+    <header className='sticky top-0 py-6 px-2 backdrop-blur-sm'>
         <nav className='flex items-center justify-between'>
             <ul className='flex gap-10 text-[1,2rem] text-zinc-400'>
                 <li><Link href={"/"}>home</Link></li>
