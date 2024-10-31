7e92be33909b72746a9b129b4f2669032f2706e7
sofisalmanarif <sofisalman9906@gmail.com>
2024-10-31 20:24:16 +0530

    toast z-index set

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Toast = () => {
   return (
-    <div className="toast rounded-lg toast-top toast-start">
+    <div className="toast z-20 rounded-lg toast-top toast-start">
   <div className="alert alert-success">
     <span>Message sent successfully.</span>
   </div>
